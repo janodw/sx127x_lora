7cb0e6e49d8d4a962906e1cd35b91194a302f148
Adam Kloboucnik <adam@klob.cz>
2020-10-29 12:23:30 +0100

    Fix fsk/ook pa_ramp methods

--- src/lib.rs
+++ src/lib.rs
@@ -670,13 +670,14 @@ where
 
     pub fn put_in_fsk_mode(&mut self) -> Result<(), Error<E, CS::Error, RESET::Error>> {
         // Put in FSK mode
-        let op_mode: &mut u8 = 0x0
-            .set_bit(7, false)  // FSK mode
-            .set_bits(5..6, 0x00)   // FSK modulation
-            .set_bit(3, false)  //Low freq registers
+        let mut op_mode: u8 = 0x0;
+        op_mode
+            .set_bit(7, false) // FSK mode
+            .set_bits(5..6, 0x00) // FSK modulation
+            .set_bit(3, false) //Low freq registers
             .set_bits(0..2, 0b011); // Mode
 
-        self.write_register(Register::RegOpMode as u8, *op_mode)
+        self.write_register(Register::RegOpMode as u8, op_mode)
     }
 
     pub fn set_fsk_pa_ramp(
@@ -684,11 +685,12 @@ where
         modulation_shaping: FskDataModulationShaping,
         ramp: FskRampUpRamDown,
     ) -> Result<(), Error<E, CS::Error, RESET::Error>> {
-        let pa_ramp: &mut u8 = 0x0
+        let mut pa_ramp: u8 = 0x0;
+        pa_ramp
             .set_bits(5..6, modulation_shaping as u8)
             .set_bits(0..3, ramp as u8);
 
-        self.write_register(Register::RegPaRamp as u8, *pa_ramp)
+        self.write_register(Register::RegPaRamp as u8, pa_ramp)
     }
 }
 /// Modes of the radio and their corresponding register values.
